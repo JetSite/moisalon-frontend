a5044ce241fd83e15bbb792b7afc4c7fc47e7cb0
Andrey <andreyshamray86@gmail.com>
2025-08-12 12:35:48 +0200

    ref: updated sitemap config and sitemap gen

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,20 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
-const dev = process.env.NEXT_PUBLIC_ENV !== 'production'
 const config = {
-  siteUrl: dev ? 'https://redesign.moi.salon/' : 'https://moi.salon/',
-  generateRobotsTxt: dev ? true : false,
-  robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: '*',
-        disallow: '/',
-      },
-      {
-        userAgent: 'undefined',
-        disallow: ['/dynamic'],
-      },
-    ],
-  },
+  siteUrl: 'https://moi.salon',
+  generateRobotsTxt: false,
   transform: async (config, path) => {
     const str = path.replace('/dynamic', '')
     return {
